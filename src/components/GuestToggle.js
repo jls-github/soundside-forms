6826d75cecb601908acf8686a702f5f419f93a49
John <souzajohnlee@gmail.com>
2021-08-31 17:18:59 -0700

    style guest toggle

--- src/components/GuestToggle.js
+++ src/components/GuestToggle.js
@@ -1,28 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function GuestToggle({ isGuest, setIsGuest }) {
+  const GuestToggleHeader = styled.h3`
+    text-align: center;
+  `;
+
+  const ToggleInputs = styled.div`
+    text-align: center;
+  `
+
+  const GuestToggleContainer = styled.div`
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  `
+
   return (
-    <div>
-      <input
-        checked={isGuest === true}
-        onClick={() => setIsGuest(true)}
-        type="radio"
-        id={true}
-        name="is-guest"
-        value="Guest"
-      />
-      <label for="is-guest">Guest</label>
-      <br />
-      <input
-        checked={isGuest === false}
-        onClick={() => setIsGuest(false)}
-        type="radio"
-        id={false}
-        name="is-guest"
-        value="Member"
-      />
-      <label for="is-guest">Member</label>
-      <br />
-    </div>
+    <GuestToggleContainer>
+      <GuestToggleHeader>Are you a guest with us today?</GuestToggleHeader>
+      <ToggleInputs>
+        <input
+          checked={isGuest === true}
+          onClick={() => setIsGuest(true)}
+          type="radio"
+          id={true}
+          name="is-guest"
+          value="Guest"
+        />
+        <label for="is-guest">Guest</label>
+        <input
+          checked={isGuest === false}
+          onClick={() => setIsGuest(false)}
+          type="radio"
+          id={false}
+          name="is-guest"
+          value="Member"
+        />
+        <label for="is-guest">Member</label>
+      </ToggleInputs>
+    </GuestToggleContainer>
   );
 }
