264ea5527056749748abb15508196e6d3d5dfce4
John <souzajohnlee@gmail.com>
2021-09-01 22:04:51 -0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import GeneralForm from "./components/GeneralForm";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <GeneralForm />
+      <Router>
+        <Switch>
+          <Route exact path="/general">
+            <GeneralForm />
+          </Route>
+          <Route exact path="/submissions">
+            <div>Submissions Page</div>
+          </Route>
+          <Route path="*">
+            <div>No form found at this url</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
