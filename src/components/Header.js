2474f3ba82b95829af6c28b829d7a885085e3850
John <souzajohnlee@gmail.com>
2021-09-06 21:33:55 -0700

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import Logo from "../images/soundside-logo.png";
+import Logo from "../images/soundside-logo-white.png";
 
 const HeaderDiv = styled.header`
-  background: green;
+  background: #3a5174;
   height: 2rem;
   padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid black;
+  box-shadow: 1px 1px 5px black;
 `;
 
 const Img = styled.img`
